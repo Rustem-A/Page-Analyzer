5a73cced825597d2bbcc754f1918989250580ab7
Rustem-A <faustyanishe@gmail.com>
2020-04-16 19:56:57 +0300

    fixed errors output

--- app/Http/Controllers/DomainsController.php
+++ app/Http/Controllers/DomainsController.php
@@ -6,6 +6,7 @@ use Illuminate\Http\Request;
 use App\Domains;
 use GuzzleHttp\Client;
 use DiDom\Document;
+use Session;
 
 class DomainsController extends Controller
 {
@@ -30,10 +31,14 @@ class DomainsController extends Controller
     public function store(Request $request)
     {
         $messages = array(
-            'url.required' => 'Поле должно быть заполнено.',
-            'url.min' => 'Url должен быть от :min символов.',
-            'url.regex' => 'Неверный формат url.',
-            'url.active_url' => 'Сайт не доступен.',
+            'url.required' =>
+            (Session::get('locale') === 'ru') ? 'Поле должно быть заполнено' : 'The field must be filled',
+            'url.min' =>
+            (Session::get('locale') === 'ru') ? 'Url должен быть от :min символов.' : 'Url must be from :min characters.',
+            'url.regex' =>
+            (Session::get('locale') === 'ru') ? 'Неверный формат url.' : 'Invalid url format',
+            'url.active_url' =>
+            (Session::get('locale') === 'ru') ? 'Сайт не доступен.' : 'The site cant be reached'
         );
         $this->validate($request, [
             'url' => 'required|min:5|active_url|regex:/^http(s)?:\/\/([a-z0-9]{2,63}\.)?[a-z0-9]{2,63}\.[a-z]{2,6}\/?$/'
--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -20,8 +20,6 @@ class Kernel extends HttpKernel
         \Illuminate\Foundation\Http\Middleware\ValidatePostSize::class,
         \App\Http\Middleware\TrimStrings::class,
         \Illuminate\Foundation\Http\Middleware\ConvertEmptyStringsToNull::class,
-        \Illuminate\Session\Middleware\StartSession::class,
-        \App\Http\Middleware\Locale::class,
     ];
 
     /**
@@ -38,6 +36,8 @@ class Kernel extends HttpKernel
             \Illuminate\View\Middleware\ShareErrorsFromSession::class,
             \App\Http\Middleware\VerifyCsrfToken::class,
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
+            \Illuminate\Session\Middleware\StartSession::class,
+            \App\Http\Middleware\Locale::class,
         ],
 
         'api' => [
