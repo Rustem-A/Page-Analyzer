97c6af01f014d76bf21f947774aa134949e25650
Rustem-A <faustyanishe@gmail.com>
2020-05-01 11:26:39 +0300

    fix

--- tests/DomainsTest.php
+++ tests/DomainsTest.php
@@ -1,14 +1,29 @@
 <?php
 
-namespace Tests;
+namespace App;
 
+use GuzzleHttp\Client;
+use GuzzleHttp\Handler\MockHandler;
+use GuzzleHttp\HandlerStack;
+use GuzzleHttp\Psr7\Response;
+use GuzzleHttp\Psr7\Request;
+use GuzzleHttp\Exception\RequestException;
 use PHPUnit\Framework\TestCase;
 
 class DomainsTest extends TestCase
 {
-    // public function testIndex()
+    // protected function setUp(): void
     // {
-    //     $this->get(route('domains.index'));
-    //     $this->seeStatusCode(200);
+    //     parent::setUp();
+    //     $path = 'tests/fixtures/testSite.html';
+    //     $body = file_get_contents($path);
+    //     $mock = new MockHandler([
+    //         new Response(200, ['Content-Length' => 11], $body)
+    //     ]);
+    //     $handler = HandlerStack::create($mock);
+    //     $this->app->bind('GuzzleClient', function ($app) use ($handler) {
+    //         return new Client(['handler' => $handler]);
+    //     });
     // }
+
 }
